eb0be3e82ea922730a1e1985eb59c94029acca53
xeyong <recruyong@gmail.com>
2021-05-08 16:56:27 +0900

    Type:Feat
    mustache 템플릿 index 생성
    
    seyong

---
+++ src/main/java/com/prac/springboot/web/IndexController.java
@@ -0,0 +1,12 @@
+package com.prac.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller//머스타치 스타터 덕분에 매핑에서 경로와 뒤의 파일확장자는 자동으로지정
+public class IndexController {
+    @GetMapping("/")//src/main/resources/templates로 오는거고
+    public String index(){
+        return "index";//index.mustache가 붙음
+    }
+}
